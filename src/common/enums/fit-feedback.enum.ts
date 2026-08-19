85de3387bdd230d48743ebf78d498a038e691d0d
bagussam <bagus.samudroal@gmail.com>
2026-08-19 16:34:16 +0700

    Add FitFeedback enum for product fit assessment
    This enum defines the allowed values for product fit assessment feedback, which are used in review creation and product detail aggregation.

---
+++ src/common/enums/fit-feedback.enum.ts
@@ -0,0 +1,9 @@
+/**
+ * Defines the allowed values for product fit assessment feedback.
+ * Used in review creation and product detail aggregation.
+ */
+export enum FitFeedback {
+  RUNS_SMALL = 'RUNS_SMALL',
+  TRUE_TO_SIZE = 'TRUE_TO_SIZE',
+  RUNS_LARGE = 'RUNS_LARGE',
+}
